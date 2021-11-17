0126d20ae38fd5e3f69c818cdeacd385ca59f043
kitoko2 <81537647+kitoko2@users.noreply.github.com>
2021-11-17 21:37:32 +0000

    modif

--- testmysql/lib/main.dart
+++ testmysql/lib/main.dart
@@ -1,10 +1,7 @@
-// ignore_for_file: prefer_const_constructors, unnecessary_new, avoid_unnecessary_containers, avoid_print
+// ignore_for_file: prefer_const_constructors, unnecessary_new, avoid_unnecessary_containers, avoid_print, sized_box_for_whitespace
 
-import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
-import "package:http/http.dart" as http;
-import 'package:testmysql/con_ins.dart';
 import 'package:testmysql/connection.dart';
 import 'package:testmysql/inscription.dart';
 import "package:flutter_gen/gen_l10n/app_localizations.dart";
@@ -18,7 +15,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    precacheImage(AssetImage('asset/3.jpg'), context);
+    // precacheImage(AssetImage('asset/3.jpg'), context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: ConIns(),
@@ -33,92 +30,143 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHome extends StatefulWidget {
-  const MyHome({Key? key}) : super(key: key);
+class ConIns extends StatefulWidget {
+  const ConIns({Key? key}) : super(key: key);
 
   @override
-  _MyHomeState createState() => _MyHomeState();
+  _ConInsState createState() => _ConInsState();
 }
 
-class _MyHomeState extends State<MyHome> {
-  TextEditingController emailcontroller = TextEditingController();
-  TextEditingController passcontroller = TextEditingController();
-  bool first = true;
+class _ConInsState extends State<ConIns> {
+  var theImg = Image.asset(
+    "asset/1.jpg",
+    fit: BoxFit.cover,
+  );
 
-  inscription(String? email, String? pass, String? image) async {
-    try {
-      var url = Uri.parse("https://poyo200.000webhostapp.com/signup.php");
+  @override
+  void didChangeDependencies() {
+    // precacheImage(theImg.image, context);
 
-      var res = await http.post(
-        url,
-        body: {"email": email, "pass": pass, "image": image},
-      );
-      if (res.statusCode == 200) {
-        var result = jsonDecode(res.body);
-        print(result);
-      }
-      print(res.statusCode);
-    } catch (e) {
-      print(e.toString());
-    }
+    super.didChangeDependencies();
   }
-
-  bool load = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: new AppBar(
-        title: Text(first ? "Inscription" : "Connection"),
-      ),
-      body: SingleChildScrollView(
-        child: load
-            ? Center(
-                child: CircularProgressIndicator(),
-              )
-            : Container(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextFormField(
-                      controller: emailcontroller,
-                      decoration: InputDecoration(
-                        hintText: "email",
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    TextFormField(
-                      controller: passcontroller,
-                      decoration: InputDecoration(
-                        hintText: "password",
-                      ),
-                    ),
-                    SizedBox(height: 15),
-                    ElevatedButton(
-                      onPressed: () async {},
-                      child: Text(first ? "S'inscrire" : "Se connecter"),
-                    ),
-                    Row(
-                      children: [
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: theImg,
+            ),
+            Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      children: const [
                         Text(
-                          first
-                              ? "Vous avez d'ja un compte?"
-                              : " vous n'avez pas de compte?",
+                          "Bienvenue chez LogementCI",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w800,
+                          ),
                         ),
-                        TextButton(
-                          onPressed: () {
-                            setState(() {
-                              first = !first;
-                            });
+                        Padding(
+                          padding: EdgeInsets.only(top: 8),
+                          child: Text(
+                            "Vous logez, notre objectif !",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.w800,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) {
+                                return Connexion();
+                              }),
+                            );
                           },
-                          child: Text(first ? "se connecter" : "s'inscrire"),
+                          child: connexionButton(),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) {
+                                return Inscription();
+                              }),
+                            );
+                          },
+                          child: inscriptionButton(),
                         ),
                       ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget connexionButton() {
+    return Center(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 45, vertical: 20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Text(
+          "Connexion",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget inscriptionButton() {
+    return Center(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 45, vertical: 20),
+        decoration: BoxDecoration(
+          border: Border.all(width: 3, color: Colors.grey),
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Text(
+          "Inscription",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
       ),
     );
   }
